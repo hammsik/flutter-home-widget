944b2ed319654499d6d3f38fcbede42af2afe02f
hammsik <asbbrp@gmail.com>
2024-07-17 19:12:56 +0900

    feat: jetpack glance로 위젯 작성시도

--- android/app/src/main/java/com/example/flutter_home_widget/ExampleAppWidget.kt
+++ android/app/src/main/java/com/example/flutter_home_widget/ExampleAppWidget.kt
@@ -2,14 +2,16 @@ package com.example.flutter_home_widget
 
 import android.content.Context
 import HomeWidgetGlanceWidgetReceiver
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.Button
 import androidx.glance.GlanceId
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.provideContent
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
+import androidx.glance.background
+import androidx.glance.appwidget.*
 import androidx.glance.text.Text
-
-/**
- * Implementation of App Widget functionality.
- */
+import androidx.glance.layout.*
 
 class ExampleAppWidgetReceiver : HomeWidgetGlanceWidgetReceiver<ExampleAppWidget>() {
     override val glanceAppWidget = ExampleAppWidget()
@@ -24,7 +26,29 @@ class ExampleAppWidget : GlanceAppWidget() {
 
         provideContent {
             // create your AppWidget here
-            Text("Hello World")
+            GlanceTheme {
+                MyContent()
+            }
+        }
+    }
+
+    @Composable
+    private fun MyContent() = Column(
+        modifier = GlanceModifier.fillMaxSize()
+            .background(GlanceTheme.colors.background),
+        verticalAlignment = Alignment.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
+        Row(horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(
+                text = "Home",
+                onClick = {}
+            )
+            Button(
+                text = "Work",
+                onClick = {}
+            )
         }
     }
 }
